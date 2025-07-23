10f4e0245a89566657bc4e570bee6cf574a69892
Max29087 <topusorker087@gmai.com>
2025-07-23 17:41:41 +0600

    Add some  new line

--- Async.js
+++ Async.js
@@ -8,6 +8,7 @@
 //         }
 //     }, 2000)
 // }
+
 // getData(1, () => {
 //     getData(2, () => {
 //         getData(3, () => {
@@ -17,33 +18,49 @@
 // });
 
 
+
+
+
 //Promises Chain
-function getData1(Id) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Data: ", Id);
-            resolve("success");
-        }, 2000);
-    });
-}
-getData1(1)
-    .then((res) => {
-        return getData1(2);
-    })
-    .then((res) => {
-        return getData1(3);
-    })
-    .then((res) => {
-        console.log(res);
-    })
+
+// function getData1(Id) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Data: ", Id);
+//             resolve("success");
+//         }, 2000);
+//     });
+// }
+
+// getData1(1)
+//     .then((res) => {
+//         return getData1(2);
+//     })
+//     .then((res) => {
+//         return getData1(3);
+//     })
+//     .then((res) => {
+//         console.log(res);
+//     })
+
+
+
 
 
 //Async
-// function hello() {
-//     console.log("Hello");
+
+// function Api(Id) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Data: ", Id);
+//             resolve(200);
+//         }, 2000)
+//     })
 // }
-// console.log("One");
-// console.log("two");
-// setTimeout(hello, 2000);
-// console.log("three");
-// console.log("Four");
+
+// (async function () {            // Call the function using IIFE
+//     await Api(1);
+//     await Api(2);
+//     await Api(3);
+//     await Api(4);
+// })();
